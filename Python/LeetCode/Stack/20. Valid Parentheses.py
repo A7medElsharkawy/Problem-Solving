50e1bc25582c9323d66a180e17635c8ebebb3cbf
A7medElsharkawy <sharkawynoo271@gmail.com>
2026-01-21 00:51:01 -0700

    add Vaild Parenteses

---
+++ Python/LeetCode/Stack/20. Valid Parentheses.py
@@ -0,0 +1,26 @@
+"""
+Link: https://leetcode.com/problems/valid-parentheses/description/
+"""
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        closeToOpen = { ")" : "(", "]" : "[", "}" : "{" }
+
+        for c in s:
+            if c in closeToOpen:
+                if stack and stack[-1] == closeToOpen[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+
+        return True if not stack else False
+            
+
+        
+"""
+Time Complexity: O(n)
+Space Complexity: O(n)
+"""
