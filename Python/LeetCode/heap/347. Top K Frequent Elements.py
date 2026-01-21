d38b85283f90760d490388863aa0ef5d110743c4
A7medElsharkawy <sharkawynoo271@gmail.com>
2026-01-20 20:02:09 -0700

    solution with bucket list faster than heap

--- Python/LeetCode/heap/347. Top K Frequent Elements.py
+++ Python/LeetCode/heap/347. Top K Frequent Elements.py
@@ -22,6 +22,35 @@ class Solution:
     
 """
 Time Complexity: O(n log k)
-Space Complexity : O(n)
+Space Complexity : O(k)
 """
 
+
+"""
+solution 2: with bucket sort
+"""
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        
+        res = [[] for _ in range(len(nums) + 1)]
+
+        dic = {}
+        for i in nums:
+            dic[i] = dic.get(i,0) + 1
+        
+        for ks,vs in dic.items():
+            res[vs].append(ks)
+        fres = []
+        for bucket in reversed(res):
+            if bucket:
+                fres.extend(bucket)
+                if len(fres) >= k:
+                    break
+
+        return fres
+
+"""
+Time Complexity: O(n)
+Space Complexity : O(k)
+"""
